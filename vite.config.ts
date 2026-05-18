9c69c94d983702050a5fafde01606738a9e2929e
Duy Le <letrankhuongduy@gmail.com>
2026-05-17 21:54:12 -0400

    feat: add path alias support in Vite and TypeScript configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,17 @@
 import { defineConfig } from 'vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+      '@assets': path.resolve(__dirname, './src/assets'),
+      '@components': path.resolve(__dirname, './src/components'),
+    },
+  },
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] })
