bcf662983d0001928846e78834e1aeb27d10b0f7
Duy Le <letrankhuongduy@gmail.com>
2026-08-09 17:27:35 -0400

    feat: Home page

---
+++ src/pages/Home.tsx
@@ -0,0 +1,13 @@
+import Currently from "@/components/Currently"
+import Hero from "@/components/Hero"
+import Latest from "@/components/Latest"
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Currently />
+      <Latest />
+    </>
+  )
+}
