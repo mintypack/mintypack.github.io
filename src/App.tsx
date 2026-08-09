3fe95d682a529d18058f7a6025388e2bda7b1d0b
Duy Le <letrankhuongduy@gmail.com>
2026-08-09 18:17:29 -0400

    chore: commented out Latest component import, currently wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
 import { Route, Routes } from "react-router"
 
 import Header from "@/components/Header"
-import ScrollToTop from "@/components/ScrollToTop"
 import About from "@/pages/About"
 import Home from "@/pages/Home"
-import NotFound from "@/pages/NotFound"
-import Post from "@/pages/Post"
-import Projects from "@/pages/Projects"
+// import NotFound from "@/pages/NotFound"
+// import Post from "@/pages/Post"
+// import Projects from "@/pages/Projects"
 
 function App() {
   return (
     <>
-      <ScrollToTop />
+      {/*<ScrollToTop />*/}
       <Header />
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/projects/:slug" element={<Post />} />
-          <Route path="*" element={<NotFound />} />
+          {/*<Route path="/projects" element={<Projects />} />*/}
+          {/*<Route path="/projects/:slug" element={<Post />} />*/}
+          {/*<Route path="*" element={<NotFound />} />*/}
         </Routes>
       </main>
     </>
